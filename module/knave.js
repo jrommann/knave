3b4684a7c23716e99ed9f6514224b9cd30b0d5ba
Jacob Rommann <jrommann@gmail.com>
2020-05-01 09:56:24 -0700

    starting over

---
+++ module/knave.js
@@ -0,0 +1,47 @@
+// Import Modules
+import { KnaveActor } from "./actor/actor.js";
+import { KnaveActorSheet } from "./actor/actor-sheet.js";
+import { KnaveItem } from "./item/item.js";
+import { KnaveItemSheet } from "./item/item-sheet.js";
+
+Hooks.once('init', async function() {
+
+  game.knave = {
+    KnaveActor,
+    KnaveItem
+  };
+
+  /**
+   * Set an initiative formula for the system
+   * @type {String}
+   */
+  CONFIG.Combat.initiative = {
+    formula: "1d20",
+    decimals: 2
+  };
+
+  // Define custom Entity classes
+  CONFIG.Actor.entityClass = KnaveActor;
+  CONFIG.Item.entityClass = KnaveItem;
+
+  // Register sheet application classes
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("knave", KnaveActorSheet, { makeDefault: true });
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("knave", KnaveItemSheet, { makeDefault: true });
+
+  // If you need to add Handlebars helpers, here are a few useful examples:
+  Handlebars.registerHelper('concat', function() {
+    var outStr = '';
+    for (var arg in arguments) {
+      if (typeof arguments[arg] != 'object') {
+        outStr += arguments[arg];
+      }
+    }
+    return outStr;
+  });
+
+  Handlebars.registerHelper('toLowerCase', function(str) {
+    return str.toLowerCase();
+  });
+});
